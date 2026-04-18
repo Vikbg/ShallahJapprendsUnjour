07ccb96068a647305f3de38a3acbc77308d718ae
Vikbg <viktorserhiienko12@gmailcom>
2026-04-18 17:53:26 +0200

    Updated VAOs and VBOs

--- include/engine.hpp
+++ include/engine.hpp
@@ -118,10 +118,18 @@ private:
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, modelVertices.size() * sizeof(Vertex), modelVertices.data(), GL_STATIC_DRAW);
 
+        // pos
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, r));
+
+        // normals
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, nx));
         glEnableVertexAttribArray(1);
+
+        // color
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, r));
+        glEnableVertexAttribArray(2);
+
         glBindVertexArray(0);
     }
 
