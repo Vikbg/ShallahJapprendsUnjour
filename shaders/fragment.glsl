53e63797515158b8ddae1b88b8e2ee5537f84409
Vikbg <viktorserhiienko12@gmailcom>
2026-04-18 18:09:18 +0200

    Updated vertex and fragment shader to support phong model

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,12 +1,38 @@
 #version 330 core
 
 in vec3 Color;
+in vec3 Normal;
 in vec3 FragPos;
+
+uniform vec3 lightPos;      // light pos in world-space
+uniform vec3 lightColor;    // rgb color
+uniform vec3 viewPos;       // camera pos like yk
 
 // Output du shader
 out vec4 FragColor;
 
+// I will use the phong model for light
+
 void main()
 {
-    FragColor = vec4(Color, 1.0);
+    // ambient
+    float ambientStrengh = 0.15;
+    vec3 ambient = ambientStrengh * lightColor;
+
+    // diffuse
+    vec3 norm     = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff    = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse  = diff * lightColor;
+
+    // specular
+    float specStrength = 0.5;
+    vec3 viewDir       = normalize(viewPos - FragPos);
+    vec3 reflectDir    = reflect(-lightDir, norm);
+    float spec         = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    vec3 specular      = specStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * Color;
+
+    FragColor = vec4(result, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,20 +2,26 @@
 
 // Input vertex attributes
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 color;
 
 // Uniform variables (mêmes pour tous les vertices)
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+// pos and normals in world-space
 out vec3 FragPos;
+out vec3 Normal;
 out vec3 Color; // Couleur à passer au fragment shader
 
 void main()
 {
     vec4 worldPos = model * vec4(position, 1.0);
-    
+    FragPos = worldPos.xyz;
+
+    Normal = mat3(transpose(inverse(model))) * normal;
+
     // Transforme la position du vertex
     gl_Position = projection * view * worldPos;
     Color = color;
