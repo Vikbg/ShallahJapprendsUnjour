455b7f2d79c42ccf12af5337f79ace1be2bc5922
Vikbg <viktorserhiienko12@gmailcom>
2026-04-18 17:05:30 +0200

    Make camera.hpp a bit cleaner

--- include/camera.hpp
+++ include/camera.hpp
@@ -8,9 +8,9 @@ class Camera {
     public:
     
         glm::vec3 position; 
-        float yaw= -90.0f;  // Regarde vers -Z par défaut
-        float pitch = 0.0f;
-        float speed = 0.05f;
+        float yaw   = -90.0f;  // Regarde vers -Z par défaut
+        float pitch =   0.0f;
+        float speed =  0.05f;
         float sensitivity = 0.1f;
 
         Camera(glm::vec3 pos = {0, 0, 3}) : position(pos) { updateVectors(); }
@@ -19,9 +19,9 @@ class Camera {
             return glm::lookAt(position, position + front, up);
         }
 
-        void processMouse(loat dx, float dy)
+        void processMouse(float dx, float dy)
         {
-            yam += dx * sensitivity;
+            yaw  += dx * sensitivity;
             pitch = glm::clamp(pitch - dy * sensitivity, -89.0f, 89.0f);
             updateVectors();
         }
@@ -38,5 +38,15 @@ class Camera {
         glm::vec3 front = {0, 0, -1};
         glm::vec3 right, up;
 
-        
+        void updateVectors()
+        {
+            glm::vec3 f;
+            f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+            f.y = sin(glm::radians(pitch));
+            f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+            front = glm::normalize(f);
+            right = glm::normalize(glm::cross(front, glm::vec3(0, 1, 0)));
+            up    = glm::normalize(glm::cross(right, front));
+        }
 };
